5eb2167ae3518fd5e98bc5fd98b244918356f87f
Chris Xu <gzchrisxjh@gmail.com>
2018-12-29 23:25:03 -0500

    Add cross origin headers

--- src/server.js
+++ src/server.js
@@ -14,6 +14,13 @@ const { user, session } = require('./apis');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/user', user);
 app.use('/session', session);
 
